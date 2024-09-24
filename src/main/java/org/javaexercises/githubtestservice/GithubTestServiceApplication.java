eca5a691ba497f006e6e55461e37b0d99c766d71
cirosouza <ciro_lut@hotmail.com>
2024-09-24 20:04:14 -0300

    First commit

---
+++ src/main/java/org/javaexercises/githubtestservice/GithubTestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.javaexercises.githubtestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaexercises/githubtestservice/GithubTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaexercises.githubtestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
